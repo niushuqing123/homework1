dda0f48bbaad510e8b8b159a3771a87f62e3beae
niushuqing123 <83936348+niushuqing123@users.noreply.github.com>
2021-12-07 13:23:19 +0800

    Add files via upload

--- taichi重写圆形碰撞模拟.py
+++ taichi重写圆形碰撞模拟.py
@@ -3,14 +3,12 @@ import taichi as ti
 import numpy as np
 from taichi.lang.ops import sqrt
 
-ti.init()
+ti.init(arch=ti.gpu)
 
 #可调节的窗口的尺寸，不影响效果
-
-screen_res = (800, 600)
+# screen_res = (800, 600)
 # screen_res = (1000, 200)
-# screen_res = (400, 900)
-
+screen_res = (400, 900)
 
 
 max_particles=20000
@@ -32,44 +30,34 @@ for i in  range(max_particles):
     color[i]=colors[i%colors.size]
 
 
+#以后可以升级成3d版，做一些与其他物理模型的耦合
 
 @ti.kernel
 def solve():
     dt=0.001
-    杨氏模量=200
+    Young_modulus=2000
     for i in range(n[None]):
         for j in range(i+1,n[None]):
-            法向量=x[j]-x[i]
-            法向量模长=sqrt(法向量[0]**2+法向量[1]**2)
-            
-            # print(法向量)
+            direction_vector=x[j]-x[i]
+            direction_vector_length=sqrt(direction_vector[0]**2+direction_vector[1]**2)
 
-            if (法向量模长<=r[i] + r[j]):
-                弹性力=杨氏模量*(法向量/法向量模长)*(r[i]+r[j]-法向量模长)
+            if (direction_vector_length<=r[i] + r[j]):
+                elastic_force=Young_modulus*(direction_vector/direction_vector_length)*(r[i]+r[j]-direction_vector_length)
                 
-                v_rel = (v[i] - v[j]).dot(法向量/法向量模长)  # 两个粒子速度之差的模长大小（点积）；相对速度在两粒子方向上的投影。
+                elastic_damping = (v[i] - v[j]).dot(direction_vector/direction_vector_length)  # 两个粒子速度之差的模长大小（点积）；相对速度在两粒子方向上的投影。
 
-                v[i] += -v_rel*10 * ((法向量/法向量模长)/m[i])*dt
-                v[j] -= -v_rel*10 * ((法向量/法向量模长)/m[j])*dt
+                v[i] += -elastic_damping*10 * ((direction_vector/direction_vector_length)/m[i])*dt
+                v[j] -= -elastic_damping*10 * ((direction_vector/direction_vector_length)/m[j])*dt
                 
-
-                # v[i] += -v_rel*1 * ((法向量/法向量模长))*dt
-                # v[j] -= -v_rel*1 * ((法向量/法向量模长))*dt
-                
-
-                v[i]-=(弹性力/m[i])*dt
-                v[j]+=(弹性力/m[j])*dt
-
-                # v[i]-=(弹性力)*dt
-                # v[j]+=(弹性力)*dt
-
+                v[i]-=(elastic_force/m[i])*dt
+                v[j]+=(elastic_force/m[j])*dt
 
 
     for i in range(n[None]):
-        if(x[i][0]<r[i]):v[i][0]+=(杨氏模量*(r[i]-x[i][0])-damping*v[i][0])*dt
-        if(x[i][1]<r[i]):v[i][1]+=(杨氏模量*(r[i]-x[i][1])-damping*v[i][1])*dt
-        if(x[i][0]+r[i]>screen_res[0]):v[i][0]+=(杨氏模量*(screen_res[0]-x[i][0]-r[i])-damping*v[i][0])*dt
-        if(x[i][1]+r[i]>screen_res[1]):v[i][1]+=(杨氏模量*(screen_res[1]-x[i][1]-r[i])-damping*v[i][1])*dt
+        if(x[i][0]<r[i]):v[i][0]+=(Young_modulus*(r[i]-x[i][0])-damping*v[i][0])*dt
+        if(x[i][1]<r[i]):v[i][1]+=(Young_modulus*(r[i]-x[i][1])-damping*v[i][1])*dt
+        if(x[i][0]+r[i]>screen_res[0]):v[i][0]+=(Young_modulus*(screen_res[0]-x[i][0]-r[i])-damping*v[i][0])*dt
+        if(x[i][1]+r[i]>screen_res[1]):v[i][1]+=(Young_modulus*(screen_res[1]-x[i][1]-r[i])-damping*v[i][1])*dt
 
         
 
@@ -77,59 +65,71 @@ def solve():
         v[i][1]-=9.8*dt
         x[i]+=v[i]*dt
         
+        
+# @ti.func #add_matrix用kernel的时候add用func
 @ti.kernel
 def add(pos_x: ti.f32, pos_y: ti.f32, r1: ti.f32,vx:ti.f32,vy:ti.f32):
     num=n[None]
-    x[n[None]] = ti.Vector([pos_x, pos_y])  # 将新粒子的位置存入x
-    r[n[None]]=r1
-    v[n[None]]=ti.Vector([vx, vy])
-    m[n[None]]=r1
+    x[num] = ti.Vector([pos_x, pos_y])  # 将新粒子的位置存入x
+    r[num]=r1
+    v[num]=ti.Vector([vx, vy])
+    m[num]=r1
     n[None] += 1  # 粒子数量加一
 
 
-@ti.kernel
-def scale(k:ti.i32):
-    if r[n[None]-1]<200 and r[n[None]-1]>4:r[n[None]-1]+=k
-    m[n[None]-1]=r[n[None]-1]
-
-
-
-def add_(pos_x: ti.f32, pos_y: ti.f32):
+# @ti.kernel#gpu运行不能用kernel，cpu可以
+def add_matrix(pos_x: ti.f32, pos_y: ti.f32):
     for i in range(6):
         for j in range(6):
             add(pos_x+i*20,pos_y+j*20,10,0,0)
-   
+# @ti.kernel#gpu运行不能用kernel
+def add_tiny(pos_x: ti.f32, pos_y: ti.f32):
+    for i in range(6):
+        for j in range(6):
+            add(pos_x+i*4,pos_y+j*4,2,0,0)
+
+@ti.kernel
+def scale(k:ti.i32):
+    if r[n[None]-1]<200 and (r[n[None]-1]>4 or k>0):
+        r[n[None]-1]+=k
+        m[n[None]-1]=r[n[None]-1]
+
+@ti.kernel
+def reset():
+    for i in range(n[None]):
+        x[i] =[0, 0]
+        r[i]=0
+        v[i]=[0, 0]
+        m[i]=0
+    n[None]=0
 
 def main():
-    gui = ti.GUI("test", res=screen_res, background_color=0xFFDEAD)
+    gui = ti.GUI("Physical circular", res=screen_res, background_color=0xFFDEAD)
     p=-1
-    # for i in range(5):
-        # add(50*i,50*i,10,0,0)
-
-    # add(100,100,50,0,0)
-    # add(400,400,50,-100,50)
-    # add(400,500,50,0,0)
-    add(400,500,50,0,0)
+    add(screen_res[0]/2,screen_res[1]/2,50,0,0)
             
     while True:
 
         R=r.to_numpy()
         X = x.to_numpy()
 
-        for e in gui.get_events(ti.GUI.PRESS):  # 此for只循环一次，为了分离用户操作
+        for e in gui.get_events(ti.GUI.PRESS):  
             if e.key in [ti.GUI.ESCAPE, ti.GUI.EXIT]:
                 exit()
             elif e.key == ti.GUI.LMB:  # 鼠标左键，增加粒子
                 add(e.pos[0]*screen_res[0], e.pos[1]*screen_res[1],10,0,0)
-            elif gui.is_pressed('w'):
+            elif gui.is_pressed('w'):#对上一个圆进行缩放
                 scale(4)
             elif gui.is_pressed('s'):  
                 scale(-4)
-            elif gui.is_pressed('p'):
+            elif gui.is_pressed('p'):#左下角喷射开关
                 p*=-1
-            elif gui.is_pressed('h'):
-                add_(e.pos[0]*screen_res[0],e.pos[1]*screen_res[1])
-
+            elif gui.is_pressed('h'):#添加一堆圆
+                add_matrix(e.pos[0]*screen_res[0],e.pos[1]*screen_res[1])
+            elif gui.is_pressed('y'):#添加一堆微小的圆
+                add_tiny(e.pos[0]*screen_res[0],e.pos[1]*screen_res[1])
+            elif gui.is_pressed('r'):#清空
+                reset()
         if p==1:
             add(0.1, 0.8,10,10,0)
             
@@ -137,35 +137,12 @@ def main():
         for i in range(n[None]):
             X[i]/=screen_res
 
-        for i in range(100):
+        for i in range(30):
             solve()
  
 
-        # colors = np.array([0x068587, 0xED553B, 0xEEEEF0], dtype=np.uint32)
-        # colors = np.array([0x068587, 0xED553B, 0xEEEEF0], dtype=np.uint32)
-
-
-        # colors=colors[r.to_numpy()]
         gui.circles(X, color=color,radius=R)
 
-        # print(X)
-        # for i in range(n[None]):
-            # gui.circle(X[i], color=0X888888, radius=R[i])
-
-            # 给每个物体不同的颜色
-            
-            # gui.circle(X[i], color=colors[material.to_numpy()], radius=R[i])
-
-
-
-            # gui.circle(X[i], color=i*1200, radius=R[i])
-            # gui.circle(X[i], color=colors[i%colors.size], radius=R[i])
-
-            # print(i*1200)
-
-            # print(0x068587)
-            
-        # break
         gui.show()
 
 
